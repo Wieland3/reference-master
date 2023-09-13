4380937647839367508082a5b486984b983ec099
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-09-13 11:54:28 +0200

    Delete unused saturator plugin

--- reference_master/plugins/custom_saturator.py
+++
@@ -1,8 +0,0 @@
-import pedalboard
-import numpy as np
-from reference_master.plugins import plugin
-
-class CustomSaturator(plugin.Plugin):
-    def __init__(self):
-        super().__init__()
-        self.board = pedalboard.Pedalboard([pedalboard.Distortion(drive_db=2)])
