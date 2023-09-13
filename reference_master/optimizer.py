d08bf23e62691cf1d1377c8b2b14aedf1c1f9520
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-09-13 11:47:06 +0200

    refactor optimizer

--- reference_master/optimizer.py
+++ reference_master/optimizer.py
@@ -1,7 +1,16 @@
-from scipy.optimize import dual_annealing, direct
+from scipy.optimize import dual_annealing
 from reference_master.utils import audio_distance
 
 def objective(params, plugin, raw, sr_raw, power_ref):
+    """
+    Objective function for the optimization
+    :param params: parameters of the plugin
+    :param plugin: plugin to process with
+    :param raw: raw track to process
+    :param sr_raw: sample rate of raw track
+    :param power_ref: power spectrum of reference track
+    :return: audio distance between processed track and reference track
+    """
     plugin.set_params(params)
     audio = plugin.process(raw, sr_raw)
     distance = audio_distance.song_distance(audio, sr_raw, power_ref)
@@ -10,18 +19,15 @@ def objective(params, plugin, raw, sr_raw, power_ref):
 
 
 def dual_annealing_optimization(plugin, bounds, raw, sr_raw, power_ref, maxiter):
+    """
+    Performs a dual annealing optimization on the plugin
+    :param plugin: Plugin to use
+    :param bounds: Lower and Upper Bounds for the params
+    :param raw: Raw Track to process
+    :param sr_raw: Sample Rate of the Raw Track
+    :param power_ref: Power Spectrum of the Reference Track
+    :param maxiter: Maximum number of iterations
+    :return:
+    """
     args = (plugin, raw, sr_raw, power_ref)
-
-    #center_freq_x0 = [60, 125, 300, 1500, 1700, 2500, 2700, 3500, 3700, 4200, 6000, 10000]
-    #gain_x0 = [0 for _ in range(12)]
-    #q_x0 = [1 for _ in range(12)]
-    #x0 = np.array(gain_x0 + center_freq_x0 + q_x0)
     return dual_annealing(func=objective, bounds=bounds, args=args, maxiter=maxiter)
-
-
-def direct_optimization(plugin, bounds, raw, sr_raw, power_ref, maxiter):
-    args = (plugin, raw, sr_raw, power_ref)
-    return direct(func=objective, bounds=bounds, args=args, maxiter=maxiter, f_min=0.0)
-
-
-
