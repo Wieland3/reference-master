f7fec1723537796e86b38599f2167ecf1d3770bf
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-09-14 12:39:09 +0200

    remove platform to build image for from build command

--- docker-build-tag.sh
+++ docker-build-tag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to build and tag the docker image
-docker build --no-cache --platform linux/amd64 -t reference-master:latest .
+docker build --no-cache -t reference-master:latest .
