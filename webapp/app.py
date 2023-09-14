c9a15e7b796d349440e8c42af044e068dbb470a4
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-09-14 11:08:21 +0200

    add functionality to upload reference to webapp

--- webapp/app.py
+++ webapp/app.py
@@ -7,6 +7,7 @@ from reference_master import constants
 executor = ThreadPoolExecutor(1)
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = constants.PATH_TO_RAW_SONGS
+app.config['UPLOAD_REF_FOLDER'] = constants.PATH_TO_REFERENCE_SONGS
 
 
 @app.route('/')
@@ -17,17 +18,21 @@ def home():
 @app.route('/upload', methods = ['POST'])
 def upload_file():
     # Get request inputs
-    file = request.files['file']
+    raw_file = request.files['raw_file']
+    ref_file = request.files['ref_file']
 
     # Save the file to disk
-    filename = file.filename
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    raw_filename = raw_file.filename
+    raw_file.save(os.path.join(app.config['UPLOAD_FOLDER'], raw_filename))
+
+    ref_filename = ref_file.filename
+    ref_file.save(os.path.join(app.config['UPLOAD_REF_FOLDER'], ref_filename))
 
     # Process file in background thread
-    executor.submit(process_file, filename)
+    executor.submit(process_file, raw_filename, ref_filename)
 
     # Return a response to the user
-    return redirect(url_for('upload_done', filename=filename))
+    return redirect(url_for('upload_done', filename=raw_filename))
 
 @app.route('/uploaded/<filename>')
 def upload_done(filename):
@@ -35,18 +40,17 @@ def upload_done(filename):
 
 @app.route("/check_file/<filename>")
 def check_file(filename):
-  exists = os.path.isfile(os.path.join(constants.PATH_TO_MASTERED_SONGS, filename))
-  return jsonify(exists=exists)
+    print(filename)
+    exists = os.path.isfile(os.path.join(constants.PATH_TO_MASTERED_SONGS, filename))
+    return jsonify(exists=exists)
 
 @app.route("/download/<filename>")
 def download(filename):
   filename = os.path.join(constants.PATH_TO_MASTERED_SONGS, filename)
   return send_file(filename, as_attachment=True)
 
-def process_file(filename):
-    print("STARTING MASTERING")
-    master(filename, "11 - Circle With Me.mp3")
-    print("MASTERING DONE")
+def process_file(filename, ref_filename):
+    master(filename, ref_filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
